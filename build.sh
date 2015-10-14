73410d93d30e1ee732b71edd14b2913344074ed5
ShiJianwen <shijianwen95@gmail.com>
2015-10-14 18:10:30 +0800

    添加构建项目的shell脚本

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -r /media/sibarone/文档/工作/BaiduApp/appid1fgfzbqsyc/app
+mkdir /media/sibarone/文档/工作/BaiduApp/appid1fgfzbqsyc/app
+cp -r src /media/sibarone/文档/工作/BaiduApp/appid1fgfzbqsyc/app
+echo "everything is ok"
+
