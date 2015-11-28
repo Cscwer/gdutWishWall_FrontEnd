8a91231d99ba0ec7b11f21458f8f0c2243e08025
ShiJianwen <shijianwen95@gmail.com>
2015-11-28 15:26:41 +0800

    delete

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8/app
-cp -r src/ /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8
-mv /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8/src /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8/app
-mv /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8/app/index.html /media/sibarone/文档/工作/BaiduApp/appidbw3h3324s8/app/app.html
-echo "everything is ok"
-
